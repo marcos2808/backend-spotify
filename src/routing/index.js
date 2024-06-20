afb7ff2754200bd746aaaf446b4da50b9df338ff
marcos vejega <106937506+marcos2808@users.noreply.github.com>
2024-06-19 23:47:43 -0400

    login, register, auth

--- src/routing/index.js
+++ src/routing/index.js
@@ -1,5 +1,10 @@
 import { Router } from "express";
+import userRouter from "./userRouter.js";
+import authRouter from "./authRouter.js";
 
 const router = Router();
+
+router.use('/users', userRouter);
+router.use('/auth', authRouter);
 
 export default router;
---
+++ src/routing/userRouter.js
@@ -0,0 +1,14 @@
+import { Router } from "express";
+import UserController from "../controllers/userController.js";
+import authenticate from "../middlewares/authMiddlewares.js";
+
+const userRouter = Router();
+
+userRouter.use(authenticate);
+
+userRouter.post("/createUser", UserController.createUser);
+userRouter.delete("/deleteUser", UserController.deleteUser);
+userRouter.put("/updatePassword", UserController.updatePassword);
+userRouter.put("/updateEmail", UserController.updateEmail);
+
+export default userRouter;
